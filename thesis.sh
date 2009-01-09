8bacfc2e8717972e899582f657afd2f81750dde3
fluri <fluri@5f156c90-ebeb-0310-af33-846360f1c98e>
2009-01-09 12:36:24 +0000

    added a script to generate the thesis template for students

---
+++ thesis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# generate a zip with all necessary files to write a thesis
+
+mkdir seal_thesis_template;
+cp seal_thesis/example_thesis.tex seal_thesis_template/;
+cp seal_thesis/seal_thesis.cls seal_thesis_template/;
+cp seal.sty seal_thesis_template/;
+cp logos/seal_bw.* seal_thesis_template/;
+cp logos/logo_inf.* seal_thesis_template/;
+cp logos/seal_blue.* seal_thesis_template/;
+cp frutiger.tar.gz seal_thesis_template/
+zip -r seal_thesis_template.zip seal_thesis_template;
+rm -rf seal_thesis_template
