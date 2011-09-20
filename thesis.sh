f8600bde3946d19cb8c7aa46334a3b37322c1526
Matthias Hert <hert@ifi.uzh.ch>
2011-09-20 09:30:32 +0200

    removed frutiger font removed old uzh/ifi logos added new uzh logos updated seal.sty updated thesis.sh

--- thesis.sh
+++ thesis.sh
@@ -6,8 +6,7 @@ cp seal_thesis/example_thesis.tex seal_thesis_template/;
 cp seal_thesis/seal_thesis.cls seal_thesis_template/;
 cp seal.sty seal_thesis_template/;
 cp logos/seal_bw.* seal_thesis_template/;
-cp logos/logo_inf.* seal_thesis_template/;
+cp logos/logo_uzh.* seal_thesis_template/;
 cp logos/seal_blue.* seal_thesis_template/;
-cp frutiger.tar.gz seal_thesis_template/
 zip -r seal_thesis_template.zip seal_thesis_template;
 rm -rf seal_thesis_template
